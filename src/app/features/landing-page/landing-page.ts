e9e172c23deba450cc80f2596b70624c3f583a78
Samantha <sam.j.maclaren@gmail.com>
2026-07-04 17:29:26 -0400

    feature / add update feed to landing page

--- src/app/features/landing-page/landing-page.ts
+++ src/app/features/landing-page/landing-page.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RealmNavigator } from '../realm-navigator/realm-navigator';
+import { UpdateFeed } from '../update-feed/update-feed';
 
 @Component({
   selector: 'app-landing-page',
   imports: [
-    RealmNavigator
+    RealmNavigator,
+    UpdateFeed
   ],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
